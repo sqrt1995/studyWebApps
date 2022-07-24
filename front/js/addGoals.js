3467c31eb5d6b4e854129fb49ea178390d0b9f91
有村　航太 <arimurakouta@kota-no-MacBookPro.local>
2022-07-24 14:02:50 +0900

    修正

--- front/js/addGoals.js
+++ front/js/addGoals.js
@@ -1,36 +1,49 @@
 const addGoals = document.getElementById('addGolas');
-const week = document.getElementById('week');
+const goals = document.getElementById('goals');
 let n = 2;
 
-addGoals.addEventListener('click', function(event){
+addGoals.addEventListener('click', function (event) {
 
-    let br1 = document.createElement('br');
+    const divWeek = document.createElement('div');
+    divWeek.setAttribute('id', 'week' + n);
+    divWeek.classList.add('row');
+
+    const div1 = document.createElement('div');
+    div1.classList.add('col-sm');
+
+    const div2 = document.createElement('div');
+    div2.classList.add('col-sm');
 
     const label1 = document.createElement('label');
     const labelText1 = document.createTextNode('何を');
-    label1.setAttribute('for', 'what'+n);
+    label1.setAttribute('for', 'what' + n);
     label1.appendChild(labelText1);
-    
+
     const input1 = document.createElement("input");
     input1.setAttribute('type', 'text');
-    input1.setAttribute('id', 'what'+n);
-    input1.setAttribute('name', 'what'+n);
+    input1.setAttribute('id', 'what' + n);
+    input1.classList.add('form-control');
 
     const label2 = document.createElement('label');
     const labelText2 = document.createTextNode('どのくらい');
-    label2.setAttribute('for', 'how'+n);
+    label2.setAttribute('for', 'how' + n);
     label2.appendChild(labelText2);
-    
+
     const input2 = document.createElement("input");
     input2.setAttribute('type', 'text');
-    input2.setAttribute('id', 'how'+n);
-    input2.setAttribute('name', 'how'+n);
+    input2.setAttribute('id', 'how' + n);
+    input2.classList.add('form-control');
 
-    week.appendChild(label1);
-    week.appendChild(input1);
+    div1.appendChild(label1);
+    div1.appendChild(input1);
 
-    week.appendChild(label2);
-    week.appendChild(input2);
+    div2.appendChild(label2);
+    div2.appendChild(input2);
 
+    divWeek.appendChild(div1);
+    divWeek.appendChild(div2);
+
+    goals.appendChild(document.createElement('br'));
+    goals.appendChild(divWeek);
     n++;
 });
